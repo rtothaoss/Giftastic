dd9918a55d4648e88fb325b976d0cbbcf71e2947
Ross carmack <rosscarmack@Rosss-MBP.fios-router.home>
2019-01-13 18:35:28 -0600

    everything working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,37 +1,110 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-$('button[data-person]').on('click', function() {
-    var player = $(this).attr('data-person');
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-    player + "&api_key=dc6zaTOxFJmzC&limit=10";
+    // $('button[data-person]').on('click', function() {
+    //     var player = $(this).attr('data-person');
+    //     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+    //     player + "&api_key=dc6zaTOxFJmzC&limit=10";
 
-    $.ajax({
-        url: queryURL,
-        method: 'GET'
-    })
-    .then(function(response){
-        var results = response.data;
+    //     $.ajax({
+    //         url: queryURL,
+    //         method: 'GET'
+    //     })
+    //     .then(function(response){
+    //         var results = response.data;
 
-        console.log(response);
-        console.log(results);
+    //         console.log(response);
+    //         console.log(results);
 
-        for(var i = 0; i < results.length; i++) {
-            var gifDiv = $('<div>')
+    //         for(var i = 0; i < results.length; i++) {
+    //             var gifDiv = $('<div>')
 
-            var rating = results[i].rating;
+    //             var rating = results[i].rating;
 
-            var p = $('<p>').text('Rating: ' + rating);
+    //             var p = $('<p>').text('Rating: ' + rating);
 
-            var playerImage = $('<img>');
-            playerImage.attr("src", results[i].images.fixed_height.url);
+    //             var playerImage = $('<img>');
+    //             playerImage.attr("src", results[i].images.fixed_height.url);
 
-            gifDiv.append(p);
-            gifDiv.append(playerImage);
+    //             gifDiv.append(p);
+    //             gifDiv.append(playerImage);
 
-            $('#gifs-here').append(gifDiv);
-        }
-    })
-})
+    //             $('#gifs-here').append(gifDiv);
+    //         }
+    //     })
+    // })
+
+    var players = ['Dak Prescott', 'Phillip Rivers', 'Patrick Mahomes', 'Tyreek Hill', 'Ezekiel Elliot', 'Josh Norman'];
+
+    function showGifs() {
+
+        $('#gifs-here').empty();
+
+        var player = $(this).attr('data-person');
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+            player + "&api_key=dc6zaTOxFJmzC&limit=10";
+
+        $.ajax({
+            url: queryURL,
+            method: 'GET'
+        }).then(function(response){
+            
+            var results = response.data;
+
+            console.log(response);
+            console.log(results);
+
+            for(var i = 0; i < results.length; i++) {
+                
+                var gifDiv = $('<div>');
+
+                gifDiv.addClass('football-gifs');
+
+                var rating = results[i].rating;
+
+                var p = $('<p>').text('Rating: ' + rating);
+
+                var playerImage = $('<img>')
+                playerImage.attr('src', results[i].images.fixed_height.url);
+
+                gifDiv.append(p);
+                gifDiv.append(playerImage);
+
+                $('#gifs-here').append(gifDiv);
+            }
+        })
+    }
+
+
+    function renderButtons() {
+        $('.player-container').empty();
+
+        for( var j = 0; j < players.length; j++) {
+            var newButton = $('<button type=button class="btn btn-primary">');
+            
+            newButton.addClass('football-players')
+
+            newButton.attr('data-person', players[j])
+
+            newButton.text(players[j]);
+
+            $('.player-container').append(newButton);
+
+        };
+    }
+
+    $('#add-player').on('click', function(event) {
+        event.preventDefault();
+
+        var player = $('#player-input').val();
+
+        players.push(player);
+
+        renderButtons();
+    });
+
+    $(document).on('click', '.football-players', showGifs);
+
+    renderButtons();
 
 
 });
