afbce9463be1bd2b6a960743df8cfe34e36cf369
Ross carmack <rosscarmack@Rosss-MBP.fios-router.home>
2019-01-16 12:56:01 -0600

    needs styling

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,38 +1,5 @@
 $(document).ready(function () {
 
-    // $('button[data-person]').on('click', function() {
-    //     var player = $(this).attr('data-person');
-    //     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-    //     player + "&api_key=dc6zaTOxFJmzC&limit=10";
-
-    //     $.ajax({
-    //         url: queryURL,
-    //         method: 'GET'
-    //     })
-    //     .then(function(response){
-    //         var results = response.data;
-
-    //         console.log(response);
-    //         console.log(results);
-
-    //         for(var i = 0; i < results.length; i++) {
-    //             var gifDiv = $('<div>')
-
-    //             var rating = results[i].rating;
-
-    //             var p = $('<p>').text('Rating: ' + rating);
-
-    //             var playerImage = $('<img>');
-    //             playerImage.attr("src", results[i].images.fixed_height.url);
-
-    //             gifDiv.append(p);
-    //             gifDiv.append(playerImage);
-
-    //             $('#gifs-here').append(gifDiv);
-    //         }
-    //     })
-    // })
-
     var players = ['Dak Prescott', 'Phillip Rivers', 'Ezekiel Elliot', 'Josh Norman', 'Tyreek Hill', 'Patrick Mahomes'];
 
     function showGifs() {
@@ -64,7 +31,11 @@ $(document).ready(function () {
                 var p = $('<p>').text('Rating: ' + rating);
 
                 var playerImage = $('<img>')
-                playerImage.attr('src', results[i].images.fixed_height.url);
+                playerImage.attr('src', results[i].images.fixed_height_still.url);
+                playerImage.attr('data-still', results[i].images.fixed_height_still.url);
+                playerImage.attr('data-animate', results[i].images.fixed_height.url);
+                playerImage.attr('data-state', 'still');
+                playerImage.addClass('gif');
 
                 gifDiv.append(p);
                 gifDiv.append(playerImage);
@@ -72,7 +43,25 @@ $(document).ready(function () {
                 // $('.lead').addClass('hidden')
 
                 $('#gifs-here').append(gifDiv);
+
             }
+            
+            $('.gif').on('click', function() {
+
+                var state = $(this).attr('data-state');
+        
+                var gifButton = $(this);
+        
+                if (state === 'still') {
+                    gifButton.attr('src', gifButton.attr('data-animate'));
+                    gifButton.attr('data-state', 'animate')
+                } else if (state === 'animate') {
+                    gifButton.attr('src', gifButton.attr('data-still'));
+                    gifButton.attr('data-state', 'still')
+                };
+        
+            });
+
         })
     }
 
@@ -106,8 +95,12 @@ $(document).ready(function () {
         
     });
 
+
     $(document).on('click', '.football-players', showGifs);
 
     renderButtons();
+
+
+
 
 });
